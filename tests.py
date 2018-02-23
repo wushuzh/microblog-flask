17a34bff502df2969308ee692da49277963fda95
wushuzh <zhangjiaqiang@gmail.com>
2018-02-23 15:03:58 +0800

    add test to retrieve one user post list

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 import unittest
 from app import app, db
-from app.models import User
+from app.models import User, Post
 
 
 class UserModelCase(unittest.TestCase):
@@ -47,6 +47,40 @@ class UserModelCase(unittest.TestCase):
         self.assertFalse(u1.is_following(u2))
         self.assertEqual(u1.followed.count(), 0)
         self.assertEqual(u2.followers.count(), 0)
+
+    def test_follow_posts(self):
+        u1 = User(username='join', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        u3 = User(username='mary', email='mary@example.com')
+        u4 = User(username='david', email='david@example.com')
+        db.session.add_all([u1, u2, u3, u4])
+
+        now = datetime.utcnow()
+        p1 = Post(body='post from john', author=u1,
+                  timestamp=now + timedelta(seconds=1))
+        p2 = Post(body='post from susan', author=u2,
+                  timestamp=now + timedelta(seconds=4))
+        p3 = Post(body='post from mary', author=u3,
+                  timestamp=now + timedelta(seconds=3))
+        p4 = Post(body='post from david', author=u4,
+                  timestamp=now + timedelta(seconds=2))
+        db.session.add_all([p1, p2, p3, p4])
+        db.session.commit()
+
+        u1.follow(u2)
+        u1.follow(u4)
+        u2.follow(u3)
+        u3.follow(u4)
+        db.session.commit()
+
+        f1 = u1.followed_posts().all()
+        f2 = u2.followed_posts().all()
+        f3 = u3.followed_posts().all()
+        f4 = u4.followed_posts().all()
+        self.assertEqual(f1, [p2, p4, p1])
+        self.assertEqual(f2, [p2, p3])
+        self.assertEqual(f3, [p3, p4])
+        self.assertEqual(f4, [p4])
 
 
 if __name__ == '__main__':
