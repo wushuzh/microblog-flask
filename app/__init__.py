4a35977e6d6aeb460fc33f6c63bf75542ee1478d
wushuzh <zhangjiaqiang@gmail.com>
2018-03-15 13:37:24 +0800

    refactor error handling as a blueprint component

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,9 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
 
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -54,8 +57,8 @@ if not app.debug:
 
 @babel.localeselector
 def get_locale():
-    #return request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'es'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    #return 'es'
 
 
-from app import routes, models, errors
+from app import routes, models
