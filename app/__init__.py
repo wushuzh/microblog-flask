100d95bcda5ca222b3f0017727bebfda57c4baa8
wushuzh <zhangjiaqiang@gmail.com>
2018-03-16 13:11:08 +0800

    refactor a blueprint component handle main logic

--- app/__init__.py
+++ app/__init__.py
@@ -29,6 +29,9 @@ app.register_blueprint(errors_bp)
 from app.auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
+
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -64,4 +67,4 @@ def get_locale():
     #return 'es'
 
 
-from app import routes, models
+from app import models
